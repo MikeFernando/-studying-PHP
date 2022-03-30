959c6e4e1844f683db7a7dc2de8d5e86c68829a9
mikera <mike.silva@pixter.com.br>
2022-03-30 14:10:00 -0300

    :monocle_face: concatenação em php

---
+++ primeiros-passos/concatenacao.php
@@ -0,0 +1,10 @@
+<?php 
+
+$nome = "Mike";
+$idade = 26;
+
+// concatenação usando 'aspas' simples
+echo 'Olá meu nome é ' . $nome . ', e tenho ' . $idade . ' anos ';
+
+// concatenação usando "aspas" duplas
+echo "Olá meu nome é $nome, e tenho $idade anos";
