816cdeaa7697d4cd700ecfcd619a77263a0e22b8
mikera <mike.silva@pixter.com.br>
2022-03-30 13:55:17 -0300

    :monocle_face: tipos e variáveis em php

---
+++ primeiros-passos/operacoes.php
@@ -0,0 +1,14 @@
+<?php 
+    $idade = 20;
+    $idadeDaqui10Anos = $idade + 10;
+
+    $soma = 2 + 2;
+    $subtracao = 2 - 2;
+    $divisao = 2 / 2;
+    $multiplicao = 2 * 2;
+    $potencia = 2 ** 2;
+    $resto = 10 % 3;
+
+    
+    
+    echo $resto;
---
+++ primeiros-passos/tipos.php
@@ -0,0 +1,12 @@
+<?php 
+$idade = 26;
+
+$divisao = 10 / 3;
+$divisao = 3;
+
+$verdadeiro = true;
+$falso = false;
+
+$string = "Eu sou um texto";
+
+echo gettype($string);
