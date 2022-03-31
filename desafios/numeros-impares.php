7283c6de141bf37e980927edd0983f1ff971d409
mikera <mike.silva@pixter.com.br>
2022-03-31 15:31:01 -0300

    :rocket: loop em lista de idades com for

---
+++ desafios/numeros-impares.php
@@ -0,0 +1,11 @@
+<?php
+
+for ( $numero = 0; $numero < 100; $numero++ ) {
+
+    if ($numero % 2 != 0 ) {
+
+        echo "#$numero" . PHP_EOL;
+
+    }
+    
+}
---
+++ desafios/tabuada.php
@@ -0,0 +1,12 @@
+<?php
+
+for ($i = 1;$i <= 10; $i ++) {
+
+    $tabuada = "$i X $i = ";
+    $result = $i * $i;
+    
+    echo "$tabuada $result" .PHP_EOL;
+
+}
+
+;
