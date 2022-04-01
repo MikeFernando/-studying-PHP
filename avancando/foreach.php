787bbbcb5ac81c4e6134b8f08b694c45921d7d92
mikera <mike.silva@pixter.com.br>
2022-03-31 21:06:54 -0300

    :rocket: adicionando item no array em php

--- avancando/foreach.php
+++ avancando/foreach.php
@@ -1,23 +1,34 @@
 <?php
 
 $contasCorrentes = [
-    12345678910     => [
-        'titular'   => 'Mike Fernando',
-        'saldo'     => 2500
+    '123.456.789-10'    => [
+    'titular'           => 'Baguera da Silva',
+    'saldo'             => 2500
     ],
 
-    321345679011    => [
-        'titular'   => 'Renê Hamilton',
-        'saldo'     => 270
+    '321.345.679-11'    => [
+    'titular'           => 'Renê Hamilton',
+    'saldo'             => 270
     ],
     
-    838002010010    => [
-        'titular'   => 'Sueli Garcez',
-        'saldo'     =>  3800
+    '838.002.010-10'    => [
+    'titular'           => 'Avelina Garcez',
+    'saldo'             =>  3800
     ]
 ];
 
+$contasCorrentes['288.787.148-01'] = [
+    'titular'           => 'Sueli Garcez',
+    'saldo'             => 1212
+];
+
+$contasCorrentes['494.208.978-69'] = [
+    'titular'           => 'Mike Fernando',
+    'saldo'             => 1313
+];
+
+
 foreach ( $contasCorrentes as $key => $conta ) {
 
-    echo $key .PHP_EOL;
+    echo $key . " " . $conta['titular'] .PHP_EOL;
 }   
