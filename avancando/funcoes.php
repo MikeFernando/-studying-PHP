c6eadaea6a993bba0221203229039a165b766508
mikera <mike.silva@pixter.com.br>
2022-04-02 14:15:57 -0300

    :rocket: Strings complexas com arrays associativos em PHP

--- avancando/funcoes.php
+++ avancando/funcoes.php
@@ -1,21 +1,20 @@
 <?php
 
-function displayMessage($message) {
-
+function displayMessage( string $message)
+{
     echo $message . PHP_EOL;
-
 }
 
+function withdraw( array $conta, float $valueWithdraw ): array
+{
 
-function withdraw( $conta, $value ) {
-
-    if ($value > $conta['saldo']) {
+    if ($valueWithdraw > $conta['saldo']) {
 
         displayMessage("Você não pode sacar, saldo insuficiente!");
 
     } else {
 
-        echo $conta['saldo'] -= $value;
+        $conta['saldo'] -= $valueWithdraw;
 
     }
 
@@ -23,29 +22,47 @@ function withdraw( $conta, $value ) {
 
 }
 
+function deposit(array $conta, float $valueDeposit): array
+{
+    if ( $valueDeposit > 0 ) {
+
+        $conta['saldo'] += $valueDeposit;
+
+    } else {
+        
+        displayMessage("Só é possivel depositar valores positivos!");
+    }
+
+    return $conta;
+}
 
 $contasCorrentes = [
-    '123.456.789-10'    => [
+    '111.456.789-11'    => [
     'titular'           => 'Mike Fernando',
     'saldo'             => 2500
     ],
 
-    '321.345.679-11'    => [
+    '222.345.679-22'    => [
     'titular'           => 'Renê Hamilton',
     'saldo'             => 300
     ],
     
-    '838.002.010-10'    => [
+    '333.456.789-33'    => [
     'titular'           => 'Avelina Garcez',
     'saldo'             =>  3800
     ]
 ];
 
 
-$contasCorrentes['123.456.789-10'] = withdraw($contasCorrentes['123.456.789-10'], 500);
+$contasCorrentes['111.456.789-11'] = withdraw($contasCorrentes['111.456.789-11'], 2000);
+$contasCorrentes['222.345.679-22'] = deposit($contasCorrentes['222.345.679-22'], -700);
 
 
 foreach ( $contasCorrentes as $cpf => $conta ) {
 
-    displayMessage($cpf . " " . $conta['titular'] . " " . $conta['saldo']);
-}   
+    displayMessage(
+        "$cpf
+        {$conta['titular']}
+        {$conta['saldo']}"
+    );
+}  
